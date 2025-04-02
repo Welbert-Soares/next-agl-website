b3c0c1ba1a13fe7db9ae6d1f8b3654af5b70bc63
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2025-04-02 19:48:36 -0300

    Remove unused fragment and background div from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,6 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <>
-        <div className="flex w-full h-screen bg-emerald-500"></div>
-      </>
     </div>
   );
 }
