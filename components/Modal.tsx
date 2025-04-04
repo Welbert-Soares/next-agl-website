5f8aca2e22beb191c50026aa1d5fe2d88452e3db
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2025-04-04 19:25:28 -0300

    Refactor Modal component to remove unused dropdown items and integrate YouTube video iframe

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,11 +1,6 @@
-import React from "react";
-
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { SquarePlay } from "lucide-react";
@@ -22,13 +17,21 @@ const Modal = () => {
           <span>ASSISTA</span>
           <SquarePlay size={20} />
 
-          <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          <DropdownMenuContent className="w-94">
+            <div className="w-full relative z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto bg-black bg-opacity-50">
+              <div className="w-full bg-black/55 rounded-lg shadow-lg transition-opacity duration-300">
+                <div className="w-full">
+                  <iframe
+                    className="w-full h-48 sm:h-64 md:h-80 lg:h-96"
+                    src="https://www.youtube.com/embed/SJbU1bMiS88?si=Qm8_y2MlB6voybh5"
+                    title="YouTube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+              </div>
+            </div>
           </DropdownMenuContent>
         </Button>
       </DropdownMenuTrigger>
